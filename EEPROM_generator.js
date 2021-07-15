bc64f5b87fed0ed3f0bbb135ee397587d4eeeceb
Kuba Buda <kubabuda@gazeta.pl>
2021-07-16 01:07:14 +0200

    Modal handling is more declaratice

--- EEPROM_generator.js
+++ EEPROM_generator.js
@@ -988,33 +988,25 @@ function utypes_generator(form, od) {
 // ####################### Modal handling ####################### //
 
 var modal = {};
-var btn = {};
-var span = {};
 
 function modalSetup() {
 	// Get the modal
 	modal = document.getElementById("myModal");
-	// Get the button that opens the modal
-	btn = document.getElementById("myBtn");
-	// Get the <span> element that closes the modal
-	span = document.getElementsByClassName("close")[0];
+}
 
-	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
-		console.log("btnClick");
-		modal.style.display = "block";
-	}
+// When the user clicks the button, open the modal 
+function modalOpen() {
+	modal.style.display = "block";
+}
 
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
+function modalClose() {
 	modal.style.display = "none";
-	}
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modalClose();
   }
 }
 
