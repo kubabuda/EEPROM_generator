7c2e6a3fa3bf948c5c9618895fb3f953e8bedd1c
Kuba Buda <kubabuda@gazeta.pl>
2021-07-20 08:42:48 +0200

    ATYPE PDO flags are set for composite objects

--- EEPROM_generator.js
+++ EEPROM_generator.js
@@ -261,7 +261,7 @@ function addPdoObjectsSection(od, odSection, pdo){
 		if (!objd.pdo_mappings) {
 			objd.pdo_mappings = [];
 		}
-		// mark object as PDO mapped, if it is nod already
+		// mark object as PDO mapped, if it is not already
 		if(!isInArray(objd.pdo_mappings, mappingValue)) {
 			objd.pdo_mappings.push(mappingValue);
 		}
@@ -661,28 +661,28 @@ function objectlist_generator(form, od, indexes)
 	objectlist += '\n';
 	//SDO objects declaration
 	indexes.forEach(index => {
-		const element = od[index];
+		const objd = od[index];
 		objectlist += `\nconst _objd SDO${index}[] =\n{`;
 		
-		switch (element.otype) {
+		switch (objd.otype) {
 			case OTYPE.VAR: {
 				let el_value = '0';
-				if (element.value && element.value != 0) {
-					el_value = `0x${element.value.toString(16)}`;
+				if (objd.value && objd.value != 0) {
+					el_value = `0x${objd.value.toString(16)}`;
 				}
-				const var_objd = `\n  {0x0, DTYPE_${element.dtype}, ${get_objdBitsize(element)}, ${get_objdFlags(element)}, acName${index}, ${el_value}, ${get_objdData(element)}},`;
+				const var_objd = `\n  {0x0, DTYPE_${objd.dtype}, ${get_objdBitsize(objd)}, ${get_objdFlags(objd)}, acName${index}, ${el_value}, ${get_objdData(objd)}},`;
 
 				objectlist += var_objd;
 				break;
 			}
 			case OTYPE.ARRAY: {
-				let arr_objd = `\n  {0x00, DTYPE_${DTYPE.UNSIGNED8}, ${8}, ATYPE_RO, acName${index}_00, ${element.items.length - 1}, NULL},`; // max subindex
-				let bitsize = dtype_bitsize[element.dtype]; /* TODO what if it is array of strings? */
+				let arr_objd = `\n  {0x00, DTYPE_${DTYPE.UNSIGNED8}, ${8}, ATYPE_RO, acName${index}_00, ${objd.items.length - 1}, NULL},`; // max subindex
+				let bitsize = dtype_bitsize[objd.dtype]; /* TODO what if it is array of strings? */
 				let subindex = 0;
-				element.items.forEach(item => {
+				objd.items.forEach(item => {
 					if (subindex > 0) { 	// skip max subindex, already done
 						var subi = subindex_padded(subindex);
-						arr_objd += `\n  {0x${subi}, DTYPE_${element.dtype}, ${bitsize}, ${get_objdFlags(item)}, acName${index}_${subi}, ${item.value || 0}, ${item.data || 'NULL'}},`;
+						arr_objd += `\n  {0x${subi}, DTYPE_${objd.dtype}, ${bitsize}, ${get_objdFlags(objd)}, acName${index}_${subi}, ${item.value || 0}, ${item.data || 'NULL'}},`;
 					}
 					subindex ++;
 				});
@@ -691,13 +691,13 @@ function objectlist_generator(form, od, indexes)
 				break;
 			}
 			case OTYPE.RECORD: {
-				let rec_objd = `\n  {0x00, DTYPE_${DTYPE.UNSIGNED8}, ${8}, ATYPE_RO, acName${index}_00, ${element.items.length - 1}, NULL},`; // max subindex
+				let rec_objd = `\n  {0x00, DTYPE_${DTYPE.UNSIGNED8}, ${8}, ATYPE_RO, acName${index}_00, ${objd.items.length - 1}, NULL},`; // max subindex
 				let subindex = 0;
-				element.items.forEach(item => {
+				objd.items.forEach(item => {
 					if (subindex > 0) { 	// skip max subindex, already done
 						var subi = subindex_padded(subindex);
 						el_bitlength = dtype_bitsize[item.dtype];
-						rec_objd += `\n  {0x${subi}, DTYPE_${item.dtype}, ${el_bitlength}, ${get_objdFlags(item)}, acName${index}_${subi}, ${item.value || 0}, ${item.data || 'NULL'}},`;
+						rec_objd += `\n  {0x${subi}, DTYPE_${item.dtype}, ${el_bitlength}, ${get_objdFlags(objd)}, acName${index}_${subi}, ${item.value || 0}, ${item.data || 'NULL'}},`;
 					}
 					subindex ++;
 				});
