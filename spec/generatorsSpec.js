7268b0ff488fce1bde0d58da8e76b0f14e84bafe
Kuba Buda <kubabuda@gazeta.pl>
2021-08-20 10:00:37 +0200

    First unit test for generator method

--- spec/generatorsSpec.js
+++ spec/generatorsSpec.js
@@ -13,7 +13,27 @@ describe("generators", function() {
       var result = utypes_generator(form, od, indexes);
 
       // assert
-      expect(result).toEqual("yyy");
+    const expectedUtypes = 
+`#ifndef __UTYPES_H__
+#define __UTYPES_H__
+
+#include "cc.h"
+
+/* Object dictionary storage */
+
+typedef struct
+{
+   /* Identity */
+
+   uint32_t serial;
+
+} _Objects;
+
+extern _Objects Obj;
+
+#endif /* __UTYPES_H__ */
+`;
+      expect(result).toEqual(expectedUtypes);
       });
   });
   
