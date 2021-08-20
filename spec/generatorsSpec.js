99affd5409f72dc8527550388da97cb1e59929c1
Kuba Buda <kubabuda@gazeta.pl>
2021-08-20 20:43:52 +0200

    All generators are working in unit tests

--- spec/generatorsSpec.js
+++ spec/generatorsSpec.js
@@ -317,6 +317,135 @@ describe("generators", function() {
             expect(result).toEqual(configData);
         });
 
+        it("ecat_options_generator should generate config data", function() {
+            // arrange
+            // act
+            var result = ecat_options_generator(form, od, indexes);
+            
+            // assert
+            const ecat_options = 
+`#ifndef __ECAT_OPTIONS_H__
+#define __ECAT_OPTIONS_H__
+
+#define USE_FOE          0
+#define USE_EOE          0
+
+#define MBXSIZE          512
+#define MBXSIZEBOOT      512
+#define MBXBUFFERS       3
+
+#define MBX0_sma         0x1000
+#define MBX0_sml         MBXSIZE
+#define MBX0_sme         MBX0_sma+MBX0_sml-1
+#define MBX0_smc         0x26
+#define MBX1_sma         MBX0_sma+MBX0_sml
+#define MBX1_sml         MBXSIZE
+#define MBX1_sme         MBX1_sma+MBX1_sml-1
+#define MBX1_smc         0x22
+
+#define MBX0_sma_b       0x1000
+#define MBX0_sml_b       MBXSIZEBOOT
+#define MBX0_sme_b       MBX0_sma_b+MBX0_sml_b-1
+#define MBX0_smc_b       0x26
+#define MBX1_sma_b       MBX0_sma_b+MBX0_sml_b
+#define MBX1_sml_b       MBXSIZEBOOT
+#define MBX1_sme_b       MBX1_sma_b+MBX1_sml_b-1
+#define MBX1_smc_b       0x22
+
+#define SM2_sma          0x1600
+#define SM2_smc          0x24
+#define SM2_act          1
+#define SM3_sma          0x1A00
+#define SM3_smc          0x20
+#define SM3_act          1
+
+#define MAX_MAPPINGS_SM2 0
+#define MAX_MAPPINGS_SM3 0
+
+#define MAX_RXPDO_SIZE   512
+#define MAX_TXPDO_SIZE   512
+
+#endif /* __ECAT_OPTIONS_H__ */
+`;
+            expect(result).toEqual(ecat_options);
+        });
+
+        it("objectlist_generator should generate config data", function() {
+            // arrange
+            // act
+            var result = objectlist_generator(form, od, indexes);
+            
+            // assert
+            const objectlist = 
+`#include "esc_coe.h"
+#include "utypes.h"
+#include <stddef.h>
+
+
+static const char acName1000[] = "Device Type";
+static const char acName1008[] = "Device Name";
+static const char acName1009[] = "Hardware Version";
+static const char acName100A[] = "Software Version";
+static const char acName1018[] = "Identity Object";
+static const char acName1018_00[] = "Max SubIndex";
+static const char acName1018_01[] = "Vendor ID";
+static const char acName1018_02[] = "Product Code";
+static const char acName1018_03[] = "Revision Number";
+static const char acName1018_04[] = "Serial Number";
+static const char acName1C00[] = "Sync Manager Communication Type";
+static const char acName1C00_00[] = "Max SubIndex";
+static const char acName1C00_01[] = "Communications Type SM0";
+static const char acName1C00_02[] = "Communications Type SM1";
+static const char acName1C00_03[] = "Communications Type SM2";
+static const char acName1C00_04[] = "Communications Type SM3";
+
+const _objd SDO1000[] =
+{
+  {0x0, DTYPE_UNSIGNED32, 32, ATYPE_RO, acName1000, 5001, NULL},
+};
+const _objd SDO1008[] =
+{
+  {0x0, DTYPE_VISIBLE_STRING, 376, ATYPE_RO, acName1008, 0, "2-channel Hypergalactic input superimpermanator"},
+};
+const _objd SDO1009[] =
+{
+  {0x0, DTYPE_VISIBLE_STRING, 40, ATYPE_RO, acName1009, 0, "0.0.1"},
+};
+const _objd SDO100A[] =
+{
+  {0x0, DTYPE_VISIBLE_STRING, 40, ATYPE_RO, acName100A, 0, "0.0.1"},
+};
+const _objd SDO1018[] =
+{
+  {0x00, DTYPE_UNSIGNED8, 8, ATYPE_RO, acName1018_00, 4, NULL},
+  {0x01, DTYPE_UNSIGNED32, 32, ATYPE_RO, acName1018_01, 0, NULL},
+  {0x02, DTYPE_UNSIGNED32, 32, ATYPE_RO, acName1018_02, 700707, NULL},
+  {0x03, DTYPE_UNSIGNED32, 32, ATYPE_RO, acName1018_03, 2, NULL},
+  {0x04, DTYPE_UNSIGNED32, 32, ATYPE_RO, acName1018_04, 1, &Obj.serial},
+};
+const _objd SDO1C00[] =
+{
+  {0x00, DTYPE_UNSIGNED8, 8, ATYPE_RO, acName1C00_00, 4, NULL},
+  {0x01, DTYPE_UNSIGNED8, 8, ATYPE_RO, acName1C00_01, 1, NULL},
+  {0x02, DTYPE_UNSIGNED8, 8, ATYPE_RO, acName1C00_02, 2, NULL},
+  {0x03, DTYPE_UNSIGNED8, 8, ATYPE_RO, acName1C00_03, 3, NULL},
+  {0x04, DTYPE_UNSIGNED8, 8, ATYPE_RO, acName1C00_04, 4, NULL},
+};
+
+const _objectlist SDOobjects[] =
+{
+  {0x1000, OTYPE_VAR, 0, 0, acName1000, SDO1000},
+  {0x1008, OTYPE_VAR, 0, 0, acName1008, SDO1008},
+  {0x1009, OTYPE_VAR, 0, 0, acName1009, SDO1009},
+  {0x100A, OTYPE_VAR, 0, 0, acName100A, SDO100A},
+  {0x1018, OTYPE_RECORD, 4, 0, acName1018, SDO1018},
+  {0x1C00, OTYPE_ARRAY, 4, 0, acName1C00, SDO1C00},
+  {0xffff, 0xff, 0xff, 0xff, NULL, NULL}
+};
+`;
+            expect(result).toEqual(objectlist);
+        });
+
         it("utypes_generator should generate expected code", function() {
             // arrange
             // act
