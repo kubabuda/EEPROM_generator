2815dd03a331129fddec8617797d6b8857a96a3b
Kuba Buda <kubabuda@gazeta.pl>
2024-02-20 22:12:48 +0100

    SPI mode hex moved to separate file

--- spec/generatorsSpec.js
+++ spec/generatorsSpec.js
@@ -1271,33 +1271,3 @@ extern _Objects Obj;
     });
 });
 
-describe("hex_generator given SPImode", function () {
-  const testCases = [
-    { mode: 0 },
-    { mode: 1 },
-    { mode: 2 },
-    { mode: 3 },
-  ];
-  var form;
-  var od;
-  var indexes;
-  
-  beforeEach(function() {
-      form = buildMockFormHelper();
-      od = buildObjectDictionary(form);
-      indexes = getUsedIndexes(od);
-  });
-
-  testCases.forEach((test, index) => {
-    it(`SPImode ${test.mode} should return 05060${test.mode}44640000 (testcase: ${index + 1})`, () => {
-      // arrange
-      form.SPImode.value = test.mode;
-      
-      // act
-      var result = hex_generator(form, true);
-      
-      // assert
-      expect(result).toEqual(`05060${test.mode}44640000`);
-    });
-  });
-});
